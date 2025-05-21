9005d443518cd44f02c5f31b051dbb70fc6c922a
oszkar0 <oskszysiak@gmail.com>
2025-05-21 15:11:15 +0200

    add test and actual code for initializing game

--- src/main/java/com/oskar/scoreboard/Game.java
+++ src/main/java/com/oskar/scoreboard/Game.java
@@ -1,4 +1,18 @@
 package com.oskar.scoreboard;
 
+import lombok.Data;
+
+@Data
 public class Game {
+    private final String homeTeam;
+    private final String awayTeam;
+    private int homeScore;
+    private int awayScore;
+
+    public Game(String homeTeam, String awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeScore = 0;
+        this.awayScore = 0;
+    }
 }
--- src/test/java/com/oskar/scoreboard/GameTest.java
+++ src/test/java/com/oskar/scoreboard/GameTest.java
@@ -1,7 +1,17 @@
 package com.oskar.scoreboard;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
+    @Test
+    void shouldCreateGameWithInitialScoreZeroZero() {
+        Game game = new Game("Poland", "Germany");
 
+        assertEquals("Poland", game.getHomeTeam());
+        assertEquals("Germany", game.getAwayTeam());
+        assertEquals(0, game.getHomeScore());
+        assertEquals(0, game.getAwayScore());
+    }
 }
